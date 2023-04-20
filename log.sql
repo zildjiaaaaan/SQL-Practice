80b61b651425aca735c1c5a028f2c34ac7ec0901
unknown <zildjian.m.18@gmail.com>
2023-04-20 14:53:53 +0800

    Look into second witness named Annabel who lives somewhere at the Franklin Ave

--- log.sql
+++ log.sql
@@ -14,3 +14,11 @@ WHERE address_street_name = 'Northwestern Dr'
 ORDER BY address_number DESC
 LIMIT 1
 ;
+
+-- Look into second witness named Annabel who
+-- lives somewhere at the Franklin Ave
+SELECT *
+FROM person
+WHERE address_street_name = 'Franklin Ave'
+    AND name LIKE '%Annabel%'
+;
