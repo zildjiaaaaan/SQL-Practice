85c8a65c994dc4c2f2f5e415c183794079d7bf7d
zildjiaaaaan <zildjian.m.18@gmail.com>
2023-04-20 15:43:23 +0800

    Narrow it down by the only the phone numbers who appeared in phone calls made in July 28, 2021 that has a duration of less than a minute

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -86,3 +86,47 @@ WHERE people.license_plate IN (
         AND atm_location = 'Leggett Street'
         AND transaction_type = 'withdraw'
 );
+
+/*
++--------+-------+----------------+-----------------+---------------+----------------+---------------+
+|   id   | name  |  phone_number  | passport_number | license_plate | account_number | creation_year |
++--------+-------+----------------+-----------------+---------------+----------------+---------------+
+| 686048 | Bruce | (367) 555-5533 | 5773159633      | 94KL13X       | 49610011       | 2010          |
+| 514354 | Diana | (770) 555-1861 | 3592750733      | 322W7JE       | 26013199       | 2012          |
+| 396669 | Iman  | (829) 555-5269 | 7049073643      | L93JTIZ       | 25506511       | 2014          |
+| 467400 | Luca  | (389) 555-5198 | 8496433585      | 4328GD8       | 28500762       | 2014          |
++--------+-------+----------------+-----------------+---------------+----------------+---------------+
+*/
+
+-- With these details, I can further narrow it down by the only the phone numbers who
+-- appeared in phone calls made in July 28, 2021 that has a duration of
+-- less than a minute
+
+SELECT people.*, bank_accounts.account_number, creation_year
+FROM people
+JOIN bank_accounts ON people.id = bank_accounts.person_id
+WHERE people.license_plate IN (
+    SELECT bakery_security_logs.license_plate
+    FROM bakery_security_logs
+    WHERE year = '2021'
+        AND month = '7'
+        AND day = '28'
+        AND hour = '10'
+        AND (minute >= '15' AND minute <= '25')
+        AND activity = 'exit'
+) AND bank_accounts.account_number IN (
+    SELECT atm_transactions.account_number
+    FROM atm_transactions
+    WHERE year = '2021'
+        AND month = '7'
+        AND day = '28'
+        AND atm_location = 'Leggett Street'
+        AND transaction_type = 'withdraw'
+) AND people.phone_number IN (
+    SELECT caller
+    FROM phone_calls
+    WHERE year = '2021'
+        AND month = '7'
+        AND day = '28'
+        AND duration < '60'
+);
