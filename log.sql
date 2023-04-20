e517985cb8659fd03e132c90beb22fdea6feb6a1
unknown <zildjian.m.18@gmail.com>
2023-04-20 14:53:01 +0800

    Look into first witness who lives at the lives at the last house on 'Northwestern Dr'

--- log.sql
+++ log.sql
@@ -5,3 +5,12 @@ FROM crime_scene_report
 WHERE date = '20180115'
     AND city = 'SQL City'
 ;
+
+-- Look into first witness who lives at the
+-- lives at the last house on "Northwestern Dr"
+SELECT *
+FROM person
+WHERE address_street_name = 'Northwestern Dr'
+ORDER BY address_number DESC
+LIMIT 1
+;
