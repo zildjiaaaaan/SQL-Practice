ab581f3d70e1275df58ceaae6c57e6e17aede14d
unknown <zildjian.m.18@gmail.com>
2023-04-20 14:54:20 +0800

    Look into witnesses' transcripts

--- log.sql
+++ log.sql
@@ -22,3 +22,18 @@ FROM person
 WHERE address_street_name = 'Franklin Ave'
     AND name LIKE '%Annabel%'
 ;
+
+-- Look into witnesses' transcripts
+SELECT name, transcript
+FROM interview
+JOIN person ON interview.person_id = person.id
+WHERE (name LIKE '%Annabel%'
+    AND address_street_name = 'Franklin Ave')
+    OR interview.person_id = (
+        SELECT id
+        FROM person
+        WHERE address_street_name = 'Northwestern Dr'
+        ORDER BY address_number DESC
+        LIMIT 1
+    )
+;
